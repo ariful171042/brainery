2f6df718d89049036703e8a3fd7818d99efb4d1b
ariful171042 <arif171042@gmail.com>
2023-05-28 22:12:20 +0600

    done

--- pages/success.js
+++ pages/success.js
@@ -1,7 +1,7 @@
 import Button from "@/components/Button";
 import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 
 const SuccessPage = ({ session }) => {
@@ -16,16 +16,17 @@ const SuccessPage = ({ session }) => {
   if (!session) {
     return null;
   }
+
   return (
     <div className="wrapper py-10 min-h-screen">
       <div className="flex flex-col items-center gap-5">
-        <h2 className="text-3xl flex items-center gap-2 ">
+        <h2 className="text-3xl flex items-center gap-2">
           <span className="text-emerald-500">
             <AiOutlineCheckCircle />
-          </span>
-          You've entrolled successfull!
+          </span>{" "}
+          {"You've entrolled successfully!"}
         </h2>
-        <Button href="/orders" placeholder={"Go to your orders"} />
+        <Button href="/orders" placeholder="Got to your orders" />
       </div>
     </div>
   );
@@ -35,11 +36,12 @@ export default SuccessPage;
 
 export const getServerSideProps = async (context) => {
   const session = await getSession(context);
+
   if (!session) {
     return {
       redirect: {
         destination: "/users/login",
-        permanest: true,
+        permanent: true,
       },
     };
   }
